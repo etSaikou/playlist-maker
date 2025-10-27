a88b58e04d088e14accc2ba4470496284fb20820
Saikou <4ung88@gmail.com>
2025-10-27 23:36:59 +0700

    split#9. homework#3

--- app/src/main/java/com/saikou/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/saikou/playlistmaker/SearchActivity.kt
@@ -1,14 +1,18 @@
 package com.saikou.playlistmaker
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class SearchActivity: AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,20 +24,46 @@ class SearchActivity: AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val bottomBar = findViewById<BottomNavigationView>(R.id.vNavMenu)
+        val searchBar = findViewById<EditText>(R.id.vSearchLine)
+        val clearButton = findViewById<ImageButton>(R.id.vClearButton)
 
-    }
+        val searchWatcher = object : TextWatcher {
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.search -> {
-                
-                true
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+
             }
-            R.id.settings -> {
-                this.startActivity(Intent(this, SettingsActivity::class.java))
-                true
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+
             }
-            else -> super.onOptionsItemSelected(item)
+
+            override fun afterTextChanged(s: Editable?) {
+               clearButton.visibility =  if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
         }
+
+        clearButton.setOnClickListener {
+            searchBar.setText("")
+        }
+
+        searchBar.addTextChangedListener(searchWatcher)
+
+
+        bottomBar.visibility = View.GONE
+
+
     }
+
 }
