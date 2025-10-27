de54d864c659939731dfe7a8325561a7d58c2509
Saikou <4ung88@gmail.com>
2025-10-27 22:49:59 +0700

    split#9. homework#2

--- app/src/main/java/com/saikou/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/saikou/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.saikou.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -22,9 +24,34 @@ class SettingsActivity: AppCompatActivity() {
         }
 
         val agreementButton = findViewById<TextView>(R.id.vUserAgreementButton)
-        agreementButton.setOnClickListener {
+        val shareButton = findViewById<TextView>(R.id.vShareButton)
+        val supportButton = findViewById<TextView>(R.id.vSupportButton)
+
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.android_dev_link))
+            shareIntent.setType("text/plain")
+            startActivity(shareIntent)
+
 
         }
+
+        supportButton.setOnClickListener {
+            val supIntent = Intent(Intent.ACTION_SENDTO)
+            supIntent.data = "mailto:".toUri()
+            supIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.my_email)))
+            supIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_subject_placeholder))
+            supIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_msg_placeholder))
+            startActivity(supIntent)
+
+        }
+
+        agreementButton.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, getString(R.string.agreement_link).toUri()))
+        }
+
+
+
 
     }
 
